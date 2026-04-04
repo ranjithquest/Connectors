984d393f6aaf1812876d4850d463c5597a9a9484
Ranjith Ravi <ranjithravi@tusredrwean2s1-Lo10.network.microsoft.com>
2026-04-04 16:00:55 +0530

    Redirect root to /connectors, remove boilerplate catch-all route

--- app/[[...slug]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import BoilerplatePage from './PageClient';
-
-export function generateStaticParams() {
-  return [{ slug: ['home'] }, { slug: ['dashboard'] }];
-}
-
-export default function Page() {
-  return <BoilerplatePage />;
-}
